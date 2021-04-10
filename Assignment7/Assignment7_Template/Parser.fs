93a21d15f95348c7de47cb510553440dbc2a1ab6
Thomas Hoffmann Kilbak <thomaskilbak@gmail.com>
2021-04-10 17:40:09 +0200

    Start assignment 7

---
+++ Assignment7/Assignment7_Template/Parser.fs
@@ -0,0 +1,109 @@
+﻿module ImpParser
+
+    open Eval
+
+    (*
+
+    The interfaces for JParsec and FParsecLight are identical and the implementations should always produce the same output
+    for successful parses although running times and error messages will differ. Please report any inconsistencies.
+
+    *)
+
+    open JParsec.TextParser             // Example parser combinator library. Use for CodeJudge.
+    // open FParsecLight.TextParser     // Industrial parser-combinator library. Use for Scrabble Project.
+    
+    // Assignement 7.1
+    let pIntToChar  = pstring "intToChar"
+    let pPointValue = pstring "pointValue"
+
+    let pCharToInt  = pstring "charToInt"
+    let pToUpper    = pstring "toUpper"
+    let pToLower    = pstring "toLower"
+    let pCharValue  = pstring "charValue"
+
+    let pTrue       = pstring "true"
+    let pFalse      = pstring "false"
+    let pIsDigit    = pstring "isDigit"
+    let pIsLetter   = pstring "isLetter"
+
+    let pif       = pstring "if"
+    let pthen     = pstring "then"
+    let pelse     = pstring "else"
+    let pwhile    = pstring "while"
+    let pdo       = pstring "do"
+    let pdeclare  = pstring "declare"
+
+    // Assignment 7.2
+    let whitespaceChar = satisfy System.Char.IsWhiteSpace
+    let pletter        = satisfy System.Char.IsLetter
+    let palphanumeric  = satisfy System.Char.IsLetterOrDigit
+
+    let spaces         = many whitespaceChar
+    let spaces1        = many1 whitespaceChar
+
+    // Assignment 7.3
+    let (.>*>.) a b = a .>> spaces .>>. b
+    let (.>*>) a b  = a .>> spaces .>> b
+    let (>*>.) a b  = a >>. spaces >>. b
+
+    // Assignement 7.4
+    let parenthesise p = pchar '(' >*>. p .>*> pchar ')'
+    let curlyBrackets p = pchar '{' >*>. p .>*> pchar '}'
+
+    let pid = pstring "not implemented"
+
+    
+    let unop _ = failwith "not implemented"
+    let binop _ p1 p2 = p1 .>>. p2 // incorrect (not implemented)
+
+    let TermParse, tref = createParserForwardedToRef<aExp>()
+    let ProdParse, pref = createParserForwardedToRef<aExp>()
+    let AtomParse, aref = createParserForwardedToRef<aExp>()
+
+    let AddParse = binop (pchar '+') ProdParse TermParse |>> Add <?> "Add"
+    do tref := choice [AddParse; ProdParse]
+
+    let MulParse = binop (pchar '*') AtomParse ProdParse |>> Mul <?> "Mul"
+    do pref := choice [MulParse; AtomParse]
+
+    let NParse   = pint32 |>> N <?> "Int"
+    let ParParse = parenthesise TermParse
+    do aref := choice [NParse; ParParse]
+
+    let AexpParse = TermParse 
+
+    let CexpParse = pstring "not implemented"
+
+    let BexpParse = pstring "not implemented"
+
+    let stmntParse = pstring "not implemented"
+
+(* These five types will move out of this file once you start working on the project *)
+    type coord      = int * int
+    type squareProg = Map<int, string>
+    type boardProg  = {
+            prog       : string;
+            squares    : Map<int, squareProg>
+            usedSquare : int
+            center     : coord
+    
+            isInfinite : bool   // For pretty-printing purposes only
+            ppSquare   : string // For pretty-printing purposes only
+        }
+
+    type word   = (char * int) list
+    type square = Map<int, word -> int -> int -> int>
+
+    let parseSquareFun _ = failwith "not implemented"
+
+    let parseBoardFun _ = failwith "not implemented"
+
+    type boardFun = coord -> square option
+    type board = {
+        center        : coord
+        defaultSquare : square
+        squares       : boardFun
+    }
+
+    let parseBoardProg (bp : boardProg) = failwith "not implemented"
+
---
+++ Assignment7/Assignment7_Template/StateMonad.fs
@@ -0,0 +1,121 @@
+﻿module StateMonad
+
+    type Error = 
+        | VarExists of string
+        | VarNotFound of string
+        | IndexOutOfBounds of int
+        | DivisionByZero 
+        | ReservedName of string           
+
+    type Result<'a, 'b>  =
+        | Success of 'a
+        | Failure of 'b
+
+    type State = { vars     : Map<string, int> list
+                   word     : (char * int) list 
+                   reserved : Set<string> }
+
+    type SM<'a> = S of (State -> Result<'a * State, Error>)
+
+    let mkState lst word reserved = 
+           { vars = [Map.ofList lst];
+             word = word;
+             reserved = Set.ofList reserved }
+
+    let evalSM (s : State) (S a : SM<'a>) : Result<'a, Error> =
+        match a s with
+        | Success (result, _) -> Success result
+        | Failure error -> Failure error
+
+    let bind (f : 'a -> SM<'b>) (S a : SM<'a>) : SM<'b> =
+        S (fun s ->
+              match a s with
+              | Success (b, s') -> 
+                match f b with 
+                | S g -> g s'
+              | Failure err     -> Failure err)
+
+
+    let ret (v : 'a) : SM<'a> = S (fun s -> Success (v, s))
+    let fail err     : SM<'a> = S (fun s -> Failure err)
+
+    let (>>=)  x f = bind f x
+    let (>>>=) x f = x >>= (fun () -> f)
+
+    let push : SM<unit> = 
+        S (fun s -> Success ((), {s with vars = Map.empty :: s.vars}))
+
+    // Exercise 6.1
+    let pop : SM<unit> = 
+        S (fun s -> Success ((), {s with vars = s.vars.Tail}))
+        
+    // Exercise 6.2
+    let wordLength : SM<int> = 
+        S (fun s -> Success (s.word.Length, s))
+
+    // Exercise 6.3
+    let characterValue (pos : int) : SM<char> = 
+        S (fun s -> 
+            if (s.word.Length <= pos || pos < 0) then
+                Failure (IndexOutOfBounds pos)
+            else
+                Success (s.word.[pos] |> fst, s)    
+        ) 
+
+    // Exercise 6.4
+    let pointValue (pos : int) : SM<int> = 
+        S (fun s -> 
+            if (s.word.Length <= pos || pos < 0) then
+                Failure (IndexOutOfBounds pos)
+            else
+                Success (s.word.[pos] |> snd, s)    
+        )
+
+    let lookup (x : string) : SM<int> = 
+        let rec aux =
+            function
+            | []      -> None
+            | m :: ms -> 
+                match Map.tryFind x m with
+                | Some v -> Some v
+                | None   -> aux ms
+
+        S (fun s -> 
+              match aux (s.vars) with
+              | Some v -> Success (v, s)
+              | None   -> Failure (VarNotFound x))
+
+    // Exercise 6.5
+    let update (x : string) (v : int) : SM<unit> = 
+        let rec aux processed remaining =
+            match remaining with
+                | []      -> None
+                | m :: ms -> 
+                    match Map.tryFind x m with
+                        | Some _ -> Some (processed @ (Map.add x v m) :: ms )
+                        | None   -> aux (processed @ m :: []) ms
+        S (fun s ->
+            match (aux [] s.vars) with
+                | Some a -> Success((),{s with vars = a})
+                | None   -> Failure (VarNotFound x)
+          )
+
+    // Exercise 6.6
+    let declare (var : string) : SM<unit> = 
+        S (fun s ->
+            if (s.vars.Length = 0) then
+                Failure(IndexOutOfBounds 0)
+            elif (Set.contains var s.reserved) then
+                Failure(ReservedName var)
+            elif (Map.containsKey var s.vars.Head) then
+                Failure (VarExists var)
+            else
+                Success((),{s with vars = (Map.add var 0 s.vars.Head) :: s.vars.Tail})
+        )
+
+
+
+
+              
+
+    
